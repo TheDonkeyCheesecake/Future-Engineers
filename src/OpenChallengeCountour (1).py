db682418ed2786622a87ce9154bfeabccd7b9fc9
Brian256 <74633320+Brain256@users.noreply.github.com>
2023-06-25 19:04:55 -0400

    Update OpenChallengeCountour (1).py
    remade program with proportional wall following and two regions of interest

--- src/OpenChallengeCountour (1).py
+++ src/OpenChallengeCountour (1).py
@@ -5,6 +5,8 @@ import serial
 from time import sleep
 import RPi.GPIO as GPIO
 
+#angles: Straight - 98 degrees
+
 if __name__ == '__main__':
     
     picam2 = Picamera2()
@@ -14,38 +16,44 @@ if __name__ == '__main__':
     picam2.preview_configuration.align()
     picam2.configure("preview")
     picam2.start()
+    
+    kp = 0.0131
+    sConst = 98
 
-    points = [(120,300), (540,300), (610,320), (60,320)]
+    # order: x1, y1, x2, y2
+    ROI1 = [40, 200, 180, 275]
+    ROI2 = [520, 170, 640, 255]
     color = (0, 255, 255)
     thickness = 4
 
     width = 640
     height = 480
     
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1) 
-    ser.flush() 
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 1) 
+    ser.flush()
     
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     angle = 2090
-    speed = 1425
+    speed = 1424
+    tSpeed = 1400
 
-    angleRight = 2070
-    angleLeft = 2110
+    rightAngle = 2078
+    leftAngle = 2118
     
-    sharpRight = 2050
-    sharpLeft = 2130
+    sharpRight = 2068
+    sharpLeft = 2128
     
-    count = 0
     turns = 0
     
-    laneThresh = 1000
+    laneThresh = 10000
+    largeLaneThresh = 10000
     val = True
 
     while True:
-       if GPIO.input(5) == GPIO.LOW:
+        if GPIO.input(5) == GPIO.LOW:
             break
     
     
@@ -57,96 +65,75 @@ if __name__ == '__main__':
 
     while True: 
 
-        rightArea, leftArea, rightX, leftX = 0, 0, 0, 0
+        rightArea, leftArea = 0, 0
 
         img = picam2.capture_array()
   
-        #image = cv2.line(img, points[0], points[1], color, thickness)
-        #image = cv2.line(img, points[1], points[2], color, thickness)
-        #image = cv2.line(img, points[2], points[3], color, thickness)
-        #image = cv2.line(img, points[3], points[0], color, thickness)
+        image = cv2.line(img, (ROI1[0], ROI1[1]), (ROI1[2], ROI1[1]), color, thickness)
+        image = cv2.line(img, (ROI1[0], ROI1[1]), (ROI1[0], ROI1[3]), color, thickness)
+        image = cv2.line(img, (ROI1[2], ROI1[3]), (ROI1[2], ROI1[1]), color, thickness)
+        image = cv2.line(img, (ROI1[2], ROI1[3]), (ROI1[0], ROI1[3]), color, thickness)
+    
+        image = cv2.line(img, (ROI2[0], ROI2[1]), (ROI2[2], ROI2[1]), color, thickness)
+        image = cv2.line(img, (ROI2[0], ROI2[1]), (ROI2[0], ROI2[3]), color, thickness)
+        image = cv2.line(img, (ROI2[2], ROI2[3]), (ROI2[2], ROI2[1]), color, thickness)
+        image = cv2.line(img, (ROI2[2], ROI2[3]), (ROI2[0], ROI2[3]), color, thickness)
         
-        input = np.float32(points)
-        output = np.float32([(0,0), (width-1,0), (width-1,height-1), (0,height-1)])
+        
 
-        matrix = cv2.getPerspectiveTransform(input,output)
-        imgPerspective = cv2.warpPerspective(img, matrix, (width,height), cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT, borderValue=(0,0,0))
-
-        imgGray = cv2.cvtColor(imgPerspective, cv2.COLOR_BGR2GRAY)
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         ret, imgThresh = cv2.threshold(imgGray, 60, 255, cv2.THRESH_BINARY_INV)
 
-        contours, hierarchy = cv2.findContours(imgThresh, 
+        contours_left, hierarchy = cv2.findContours(imgThresh[ROI1[1]:ROI1[3], ROI1[0]:ROI1[2]], 
         cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        
-        count = 0
+    
+        contours_right, hierarchy = cv2.findContours(imgThresh[ROI2[1]:ROI2[3], ROI2[0]:ROI2[2]], 
+        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-        for i in range(len(contours)):
-            cnt = contours[i]
+        for i in range(len(contours_left)):
+            cnt = contours_left[i]
             area = cv2.contourArea(cnt)
             
-            cv2.drawContours(imgPerspective, contours, i, (0, 255, 0), 2)
+            cv2.drawContours(img, contours_left, i, (0, 255, 0), 2)
             approx=cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt,True),True)
             x,y,w,h=cv2.boundingRect(approx)
-            cv2.rectangle(imgPerspective,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
             
-            if area > laneThresh:
-                count += 1
+            leftArea = max(area, leftArea) 
+        
+        for i in range(len(contours_right)):
+            cnt = contours_right[i]
+            area = cv2.contourArea(cnt)
             
-            #code that stores the 2 highest areas
-            if area > leftArea: 
-              leftArea = rightArea
-              leftX = rightX
-              
-              rightArea = area
-              rightX = x
-              
-            elif area > leftArea:    
-              leftArea = area
-              leftX = x
+            cv2.drawContours(img, contours_right, i, (0, 255, 0), 2)
+            approx=cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt,True),True)
+            x,y,w,h=cv2.boundingRect(approx)
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+            
+            rightArea = max(area, rightArea)
+
+        print(leftArea, rightArea)
+        
+        aDifference = rightArea - leftArea
+        
+        angle = int(sConst + aDifference * kp) + 2000
+        
+        if sharpLeft - angle <= 10:
+            angle = sharpLeft
+        
+        if angle - sharpRight <= 10:
+            angle = sharpRight
         
         #print(count)
         
-        cv2.imshow("finalColor", imgThresh)
-          
-        if leftX > rightX: #if left area is actually the right area swap
-          leftArea, rightArea = rightArea, leftArea
-        
-        if max(leftArea, rightArea) >= 100000:
-            print("back wall detected")
-            if angle == angleRight:
-              angle = sharpRight
-            elif angle == angleLeft:
-              angle = sharpLeft
+        cv2.imshow("finalColor", img)
             
+        if sharpRight <= angle <= sharpLeft: 
+            ser.write((str(angle)).encode('utf-8'))
+            print("signal sent", angle)
         
-        elif leftArea > laneThresh and rightArea > laneThresh and count == 2: # 2 lanes are detected
-          if leftArea > rightArea:
-             # print("right, 2 lanes")
-              angle = angleRight
-            
-          else:
-              #print("left, 2 lanes")
-              angle = angleLeft
-        
-          print("reg turn")        
-            
-          val = True
-        
-        elif count >= 2 and val: #1 lane is detected
-          
-          
-          turns += 1
-          
-          val = False
-            
-
-        ser.write((str(angle)).encode('utf-8'))
-        #print("signal sent", angle) 
-        
-        sleep(0.1)
-        
-        if cv2.waitKey(1)==ord('q'):          
+        if cv2.waitKey(1)==ord('q'):      
             ser.write((str(1500)).encode('utf-8'))
             print(1500)
             ser.write((str(2090)).encode('utf-8'))
