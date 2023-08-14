874a80add75684c9e89c40bcc272f2748c141cf6
Brian256 <74633320+Brain256@users.noreply.github.com>
2023-08-14 14:38:11 -0400

    Update ObstacleChallenge.py

--- src/ObstacleChallenge.py
+++ src/ObstacleChallenge.py
@@ -39,11 +39,6 @@ def displayROI(img, ROI1, ROI2, ROI3):
     image = cv2.line(img, (ROI3[0], ROI3[1]), (ROI3[0], ROI3[3]), (0, 255, 255), 4)
     image = cv2.line(img, (ROI3[2], ROI3[3]), (ROI3[2], ROI3[1]), (0, 255, 255), 4)
     image = cv2.line(img, (ROI3[2], ROI3[3]), (ROI3[0], ROI3[3]), (0, 255, 255), 4)
-    
-    image = cv2.line(img, (ROI4[0], ROI4[1]), (ROI4[2], ROI4[1]), (0, 255, 255), 4)
-    image = cv2.line(img, (ROI4[0], ROI4[1]), (ROI4[0], ROI4[3]), (0, 255, 255), 4)
-    image = cv2.line(img, (ROI4[2], ROI4[3]), (ROI4[2], ROI4[1]), (0, 255, 255), 4)
-    image = cv2.line(img, (ROI4[2], ROI4[3]), (ROI4[0], ROI4[3]), (0, 255, 255), 4)
 
 #function to bring the car to a stop
 def stopCar():
@@ -56,7 +51,7 @@ if __name__ == '__main__':
     picam2 = Picamera2()
     picam2.preview_configuration.main.size = (640,480)
     picam2.preview_configuration.main.format = "RGB888"
-    picam2.preview_configuration.controls.FrameRate = 90
+    picam2.preview_configuration.controls.FrameRate = 60
     picam2.preview_configuration.align()
     picam2.configure("preview")
     picam2.start()
@@ -70,43 +65,34 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    
-    redTarget = 150
-    greenTarget = 490
-    
-    turnDir = "none" 
-    
     #lists storing coordinates for the regions of interest to find contours of the lanes
     # order: x1, y1, x2, y2
-    ROI1 = [25, 230, 310, 310]
-    ROI2 = [300, 185, 640, 275]
-    ROI3 = [redTarget - 30, 175, greenTarget + 30, 400]
-    ROI4 = [200, 350, 440, 400]
+    ROI1 = [45, 230, 315, 295]
+    ROI2 = [380, 205, 640, 255]
+    ROI3 = [240, 200, 500, 330]
 
     #booleans for tracking whether car is in a left or right turn
     lTurn = False
     rTurn = False
   
-    kp = 0.004 #value of proportional for proportional steering
-    kd = 0.004  #value of derivative for proportional and derivative sterring
+    kp = 0.005 #value of proportional for proportional steering
+    kd = 0.005  #value of derivative for proportional and derivative sterring
 
-    cKp = 0.2
-    cKd = 0.2
-    cy = 0.2
+    cKp = 0.05
+    cKd = 0.05
   
     straightConst = 98 #angle in which car goes straight
 
     turnThresh = 200 #if area of a lane is under this threshold car goes into a turn
-    exitThresh = 3000 #if area of both lanes is over this threshold car exits a turn
+    exitThresh = 1500 #if area of both lanes is over this threshold car exits a turn
   
     angle = 2098 #variable for the current angle of the car
     prevAngle = angle #variable tracking the angle of the previous iteration
-    tDeviation = 40 #value used to calculate the how far left and right the car turns during a turn
+    tDeviation = 33 #value used to calculate the how far left and right the car turns during a turn
     sharpRight = straightConst - tDeviation + 2000 #the default angle sent to the car during a right turn
     sharpLeft = straightConst + tDeviation + 2000 #the default angle sent to the car during a left turn
     
     speed = 1425 #variable for the speed of the car
-    targetS = 1435
     
     aDiff = 0 #value storing the difference of area between contours
     prevDiff = 0 #value storing the previous difference of contours for derivative steering
@@ -114,12 +100,12 @@ if __name__ == '__main__':
     error = 0
     prevError = 0
 
+    redTarget = 570
+    greenTarget = 70
+
     cTarget = 0
     contX = 0
-    contY = 0
-    
-    t = 0
-    tSignal = False
+    contY = 0 
 
     sleep(8) #delay 8 seconds for the servo to be ready
 
@@ -129,16 +115,11 @@ if __name__ == '__main__':
             #break
 
     #write initial values to car
-    write(speed) 
+    #write(speed) 
     write(angle)
 
     #main loop
     while True:
-        
-        if speed != targetS:
-            speed += 1
-            write(speed)
-            
 
         rightArea, leftArea = 0, 0
 
@@ -148,7 +129,7 @@ if __name__ == '__main__':
         imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         #threshold image
-        ret, imgThresh = cv2.threshold(imgGray, 65, 255, cv2.THRESH_BINARY_INV)
+        ret, imgThresh = cv2.threshold(imgGray, 45, 255, cv2.THRESH_BINARY_INV)
 
         #find left and right contours of the lanes
         contours_left, hierarchy = cv2.findContours(imgThresh[ROI1[1]:ROI1[3], ROI1[0]:ROI1[2]], 
@@ -171,176 +152,102 @@ if __name__ == '__main__':
         # convert from BGR to HSV
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Upper red mask (170-180)
-        lower_red = np.array([165, 150, 100])
-        upper_red = np.array([180, 255, 255])
-        r_mask = cv2.inRange(img_hsv, lower_red, upper_red)
- 
+        # Lower red mask (0-10)
+        lower_red = np.array([0, 100, 20])
+        upper_red = np.array([10, 255, 255])
+        r_mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
 
-        contours_red = cv2.findContours(r_mask[ROI3[1]:ROI3[3], ROI3[0]:ROI3[2]], cv2.RETR_EXTERNAL,
+        # Upper red mask (170-180)
+        lower_red = np.array([160, 100, 20])
+        upper_red = np.array([175, 255, 255])
+        r_mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+        # Join the masks
+        raw_mask_r = r_mask0 | r_mask1
+
+        contours_red = cv2.findContours(r_mask1[ROI3[1]:ROI3[3], ROI3[0]:ROI3[2]], cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)[-2]# Find contours
 
         # green mask
-        lower_green = np.array([70, 100, 40])
-        upper_green = np.array([95, 255, 255])
+        lower_green = np.array([60, 100, 20])
+        upper_green = np.array([90, 255, 255])
 
         g_mask = cv2.inRange(img_hsv, lower_green, upper_green)
 
         contours_green = cv2.findContours(g_mask[ROI3[1]:ROI3[3], ROI3[0]:ROI3[2]], cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)[-2]# Find contours
-        
-        # blue mask
-        lower_blue = np.array([100, 100, 100])
-        upper_blue = np.array([135, 255, 255])
-
-        b_mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
-
-        contours_blue = cv2.findContours(b_mask[ROI4[1]:ROI4[3], ROI4[0]:ROI4[2]], cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]# Find contours
-        
-        # orange mask
-        lower_orange = np.array([5, 100, 20])
-        upper_orange = np.array([20, 255, 255])
-
-        o_mask = cv2.inRange(img_hsv, lower_orange, upper_orange)
-
-        contours_orange = cv2.findContours(o_mask[ROI4[1]:ROI4[3], ROI4[0]:ROI4[2]], cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]# Find contours
-        
-        
-        
 
         for i in range(len(contours_green)):
           cnt = contours_green[i]
           area = cv2.contourArea(cnt)
-          #print(area)
-          if(area > 80):
+          print(area)
+          if(area > 100):
               #cv2.drawContours(img, contours_green, i, (0, 255, 0), 2)
               approx=cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt,True),True)
               x,y,w,h=cv2.boundingRect(approx)
               
-              x += ROI3[0]
-              y += ROI3[1]
-              
-              cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+              x += ROI3[0] + w / 2
+              y += ROI3[1] 
 
               if y > contY: 
                 contY = y
-                contX = x + w // 2
+                contX = x
                 cTarget = greenTarget
+              
+              cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
             
         for i in range(len(contours_red)):
           cnt = contours_red[i]
           area = cv2.contourArea(cnt)
-          #print(area)
-          if(area > 80):
+          print(area)
+          if(area > 100):
               #cv2.drawContours(img, contours_red, i, (0, 255, 0), 2)
               approx=cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt,True),True)
               x,y,w,h=cv2.boundingRect(approx)
               
-              x += ROI3[0]
+              x += ROI3[0] + w / 2
               y += ROI3[1]
-              
-              cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 
               if y > contY: 
                 contY = y
-                contX = x + w // 2
+                contX = x
                 cTarget = redTarget
-                
-        for i in range(len(contours_orange)):
-          cnt = contours_orange[i]
-          area = cv2.contourArea(cnt)
-          
-          #print(area)
-          if(area > 100):
-              if turnDir == "none":
-                  turnDir = "right"
-              #cv2.drawContours(img[ROI4[1]:ROI4[3], ROI4[0]:ROI4[2]], contours_orange, i, (0, 255, 0), 2)
-              if turnDir == "right":
-                  rTurn = True
-                  tSignal = True
-        
-        for i in range(len(contours_blue)):
-          cnt = contours_blue[i]
-          area = cv2.contourArea(cnt)
-          #print(area)
-          if(area > 100):
-              if turnDir == "none":
-                  turnDir = "left" 
-              #cv2.drawContours(img[ROI4[1]:ROI4[3], ROI4[0]:ROI4[2]], contours_blue, i, (0, 255, 0), 2)
-              if turnDir == "left":
-                  lTurn = True
-                  tSignal = True
               
+              cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
 
-        if cTarget == 0:
-            
+        if cTarget == 0: 
             aDiff = rightArea - leftArea
 
             #calculate angle using PD steering
             angle = int(straightConst + aDiff * kp + (aDiff - prevDiff) * kd) + 2000
 
             prevDiff = aDiff
-            
-            
-            
-        else:
-            
-            if (lTurn or rTurn) and not tSignal:
-                print("pillar detected, end of turn", t)
-                lTurn = False
-                rTurn = False
-                t += 1
-            
 
-            error = cTarget - contX
-            
-            angle = int(straightConst + error * cKp + (error - prevError) * cKd + cy * (contY - 170)) + 2000
-            
-            #print(angle)
-            #print(cTarget)
-            #print(contX)
-            #print(error) 
-            
+        else: 
 
-            prevError = error
-            contY = 0
-            contX = 0
-            cTarget = 0
-              
-            
+          error = contX - cTarget
+        
+          angle = int(straightConst + error * cKp + (error - prevError) * cKd) + 2000
+
+          prevError = error
+          contY = 0
+          contX = 0
+          cTarget = 0
 
         if angle != prevAngle:
-            
-            if ((rightArea >= exitThresh and rTurn) or (leftArea >= exitThresh and lTurn)) and not tSignal: 
-                  #set turn variables to false as turn is over
-                  lTurn = False 
-                  rTurn = False
-                  #increase number of turns by 1
-                  t += 1
-                  
-                  print("lane detected, end of turn", t) 
-
-            if rTurn:
-                angle = sharpRight
-                
-            elif lTurn:
-                angle = sharpLeft
-                
-            
-            write(max(min(angle, sharpLeft), sharpRight))
+            #write(max(min(angle, sharpLeft), sharpRight))
+            write(angle)
                 
 
             
-        if cv2.waitKey(1)==ord('q') or t == 12:
+        if cv2.waitKey(1)==ord('q'):
             stopCar() 
             break
 
       
 
         prevAngle = angle #update previous angle
-        tSignal = False
+        
         
         #display regions of interest
         displayROI(img, ROI1, ROI2, ROI3)
@@ -348,9 +255,5 @@ if __name__ == '__main__':
         #show image
         cv2.imshow("finalColor", img)
         
-        print(leftArea, rightArea) 
-        
     #close all image windows
     cv2.destroyAllWindows()
-
-
