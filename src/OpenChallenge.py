5a88e27cf2d40fdc92cc5ce90275b89decb6627d
Brian256 <74633320+Brain256@users.noreply.github.com>
2023-08-15 14:43:39 -0400

    Update OpenChallenge.py

--- src/OpenChallenge.py
+++ src/OpenChallenge.py
@@ -11,7 +11,7 @@ def write(value):
     ser.write((str(value)).encode('utf-8'))
     print("signal sent:", value)
 
-#function which displays the  Regions of Interest on the image
+#function which displays the regions of interest on the image
 def displayROI(img, ROI1, ROI2):
     image = cv2.line(img, (ROI1[0], ROI1[1]), (ROI1[2], ROI1[1]), (0, 255, 255), 4)
     image = cv2.line(img, (ROI1[0], ROI1[1]), (ROI1[0], ROI1[3]), (0, 255, 255), 4)
@@ -22,6 +22,11 @@ def displayROI(img, ROI1, ROI2):
     image = cv2.line(img, (ROI2[0], ROI2[1]), (ROI2[0], ROI2[3]), (0, 255, 255), 4)
     image = cv2.line(img, (ROI2[2], ROI2[3]), (ROI2[2], ROI2[1]), (0, 255, 255), 4)
     image = cv2.line(img, (ROI2[2], ROI2[3]), (ROI2[0], ROI2[3]), (0, 255, 255), 4)
+
+    image = cv2.line(img, (ROI3[0], ROI3[1]), (ROI3[2], ROI3[1]), (0, 255, 255), 4)
+    image = cv2.line(img, (ROI3[0], ROI3[1]), (ROI3[0], ROI3[3]), (0, 255, 255), 4)
+    image = cv2.line(img, (ROI3[2], ROI3[3]), (ROI3[2], ROI3[1]), (0, 255, 255), 4)
+    image = cv2.line(img, (ROI3[2], ROI3[3]), (ROI3[0], ROI3[3]), (0, 255, 255), 4)
 
 #function to bring the car to a stop
 def stopCar():
@@ -48,7 +53,7 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    #lists storing coordinates for the regions of interest to find contours of the lanes
+    #lists storing coordinates for the regions of interest to find contours of the lanes and the orange line 
     # order: x1, y1, x2, y2
     ROI1 = [65, 225, 315, 305]
     ROI2 = [380, 190, 630, 270]
@@ -59,10 +64,9 @@ if __name__ == '__main__':
     rTurn = False
   
     t = 0 #number of turns car has completed
-    i = 0 #counts the iterations of the main control loop 
     
     kp = 0.004 #value of proportional for proportional steering
-    kd = 0.004  #value of derivative for proportional and derivative sterrin
+    kd = 0.004 #value of derivative for proportional and derivative sterring
     
     straightConst = 98 #angle in which car goes straight
 
@@ -90,7 +94,8 @@ if __name__ == '__main__':
     #write initial values to car
     write(speed) 
     write(angle)
-    
+
+    #boolean tracking when the orange line on the mat is detected
     lDetected = False
 
     #main loop
@@ -101,17 +106,9 @@ if __name__ == '__main__':
 
         #get an image from pi camera
         img = picam2.capture_array()
-
-        #convert image into grayscale
-        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         
         # convert from BGR to HSV
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        
-        '''
-        #threshold image
-        ret, imgThresh = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
-        '''
         
         # black mask
         lower_black = np.array([0, 0, 0])
@@ -125,17 +122,9 @@ if __name__ == '__main__':
 
         o_mask = cv2.inRange(img_hsv, lower_orange, upper_orange)
 
-        contours_orange = cv2.findContours(o_mask[ROI4[1]:ROI4[3], ROI4[0]:ROI4[2]], cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]# Find contours
-        #cv2.imshow("black", imgThresh)
-        
-        #imgThresh = cv2.subtract(imgThresh, b_mask) 
-        
-        #cv2.imshow("blue", b_mask)
-        #cv2.imshow("subtracted", imgThresh) 
-
-        
-
+        #find contours to detect orange line
+        contours_orange = cv2.findContours(o_mask[ROI3[1]:ROI3[3], ROI3[0]:ROI3[2]], cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)[-2] 
 
         #find left and right contours of the lanes
         contours_left, hierarchy = cv2.findContours(imgThresh[ROI1[1]:ROI1[3], ROI1[0]:ROI1[2]], 
@@ -143,9 +132,12 @@ if __name__ == '__main__':
     
         contours_right, hierarchy = cv2.findContours(imgThresh[ROI2[1]:ROI2[3], ROI2[0]:ROI2[2]], 
         cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        
+      
+        '''
+        #find all contours in image for debugging
         contours, hierarchy = cv2.findContours(imgThresh, 
         cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        '''
 
         #iterate through every contour in both the left and right region of interest and take the largest one in each
         for cnt in contours_left:
@@ -157,18 +149,18 @@ if __name__ == '__main__':
             area = cv2.contourArea(cnt)
 
             rightArea = max(area, rightArea)
-            
-         for i in range(len(contours_orange)):
-          cnt = contours_orange[i]
-          area = cv2.contourArea(cnt)
-          
-          #print(area)
-          if(area > 100):
-              lDetected = True
-              
 
+        #iterate through the contours in the centre region of interest to find the orange line
+        for i in range(len(contours_orange)):
+            cnt = contours_orange[i]
+            area = cv2.contourArea(cnt)
+            
+            #if contour is detected set lDetected to true
+            if(area > 100):
+                lDetected = True
+                
+        '''
         #draw all contours in full image
-        
         for i in range(len(contours)):
             cnt = contours[i]
             area = cv2.contourArea(cnt)
@@ -176,9 +168,8 @@ if __name__ == '__main__':
             cv2.drawContours(img, contours, i, (0, 255, 0), 2)
             approx=cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt,True),True)
             x,y,w,h=cv2.boundingRect(approx)
+        '''
         
-        
-
         #calculate difference of areas between the areas of the lanes
         aDiff = rightArea - leftArea
 
@@ -189,14 +180,8 @@ if __name__ == '__main__':
         if leftArea <= turnThresh and not rTurn:
             lTurn = True
 
-
         elif rightArea <= turnThresh and not lTurn:
             rTurn = True
-
-        #if at the very start of the run, the car does a turn, remove a turn as the car is just adjusting to the center as it was placed to the side and is not in an actual turn
-        #if i == 0 and (lTurn or rTurn):
-           # t -= 1
-
 
         #if angle is different from previous angle
         if angle != prevAngle:
@@ -208,8 +193,8 @@ if __name__ == '__main__':
                   #set turn variables to false as turn is over
                   lTurn = False 
                   rTurn = False
-                  #increase number of turns by 1
                   
+                  #increase number of turns by 1 only if the orange line has been detected 
                   if lDetected: 
                       t += 1
                       lDetected = False
@@ -221,7 +206,7 @@ if __name__ == '__main__':
               elif rTurn: 
                   angle = min(angle, sharpRight)
 
-                #write angle to arduino to change servo
+              #write angle to arduino to change servo
               write(angle)
             #if not in a turn write the angle and if the angle is over sharpLeft or sharpRight values it will be rounded down to those values
             else:
@@ -231,26 +216,17 @@ if __name__ == '__main__':
         prevDiff = aDiff
             
         #stop the car and end the program if either q is pressed or the car has done 3 laps (12 turns) and is not still in the turn (does this by checking whether the angle is within 10 of straight)
-        if cv2.waitKey(1)==ord('q') or (t == 12 and abs(angle - (straightConst + 2000)) <= 15) :
+        if cv2.waitKey(1)==ord('q') or (t == 12 and abs(angle - (straightConst + 2000)) <= 10) :
             stopCar() 
             break
-
-        #display values
-        #print(leftArea, rightArea)
-        #print(t)
-        #print(angle)
-        #print(prevAngle)
         
         prevAngle = angle #update previous angle
         
         #display regions of interest
-        #displayROI(img, ROI1, ROI2)
+        #displayROI(img, ROI1, ROI2, ROI3)
 
         #show image
         #cv2.imshow("finalColor", img)
-
-        #increase the iterations by 1
-        i += 1
         
     #close all image windows
     cv2.destroyAllWindows()
